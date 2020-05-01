2230d717cd72910b636d29fcf1661d5fc101561f
AljosaZ <znidaric.aljosa@gmail.com>
2020-05-01 11:54:05 +0200

    made the conversion unit a global variable

--- unit_converter.py
+++ unit_converter.py
@@ -1,6 +1,8 @@
 print("Hello dear user, this program will convert units from kilometers into miles.")
 
 answer = "yes"
+KILOMETER_TO_MILE = 0.6213712
+
 while answer.lower() == "yes":
     kilometers = input("Please enter the number of kilometers you want to convert into miles: ")
 
@@ -9,9 +11,9 @@ while answer.lower() == "yes":
     else:
         kilometers = (float(kilometers))
 
-    miles = round(kilometers * 0.6213712, 3)
+    miles = round(kilometers * KILOMETER_TO_MILE, 3)
 
-    print(f"{kilometers} kilometers are equal to {miles} miles.\n")
+    print(f"{kilometers} kilometers are equal to {miles} miles.")
 
     answer = input("If you would like to do another conversion please enter yes: ")
 
